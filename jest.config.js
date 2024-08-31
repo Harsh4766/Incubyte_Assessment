7ff1acc7fa80d187bf591e4fb2cf5b2ce97ef785
Harsh Yadav <yadavhd04@gmail.com>
2024-08-31 14:03:41 +0530

    Failing testcase for adding a book

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  };
+  
